98a6f73b7cbbd7ecfde5e6d65e980211fea66fce
worashf <uppert83@gmail.com>
2022-06-22 01:02:13 +0300

    add card styling

--- src/module/dom-utils.js
+++ src/module/dom-utils.js
@@ -7,13 +7,15 @@ const displayAllData =(data)=>{
     //  tvWrapper.innerHTML=''
     console.log(tvWrapper)
     console.log(data[0].name)
-     const filtered = data.slice(0,8);
+     const filtered = data.slice(0,6);
   
      filtered.forEach(element => {
+     
          tvWrapper.innerHTML += `
-         <div class="card"> 
-           <img src=${element.image.medium} alt ="best"/>
-            <p> ${element.name}</p>
+         <div class="card" id=${element.id}> 
+        
+           <img src=${element.image.medium} alt ="best" class="image"/>
+            <p class="show-name"> ${element.name}</p>
          </div>
          `
      });
