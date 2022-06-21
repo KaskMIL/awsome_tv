e711b6d4f373ba84a3fb2bd0e729498b28486d01
toMilanesi <tomi.milanesi@gmail.com>
2022-06-21 15:22:52 -0300

    Fix fetch data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './styles/main.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import { getData } from './modules/dataTest';
+import { getData } from './modules/dataTest.js';
 
 const mainUrl = 'https://api.tvmaze.com/shows';
 
-
-
 getData(mainUrl).then((data) => {
-  console.log(data.length)
-})
+  data.forEach(element => {
+    if(element.id == 3) {
+      console.log(element.summary)
+    }
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -25,13 +25,13 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
     ],
   },
   plugins: [
