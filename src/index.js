ee3d7f409a237127abbb93417be044fa62ba4a4c
toMilanesi <tomi.milanesi@gmail.com>
2022-06-22 09:40:08 -0300

    Fix get-set coments, get-set likes

--- src/index.js
+++ src/index.js
@@ -1,65 +1,7 @@
 import './styles/main.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import { getData, setLike } from './modules/dataTest.js';
+import { getData, setLike, getLikes } from './modules/dataTest.js';
 
 const mainUrl = 'https://api.tvmaze.com/shows';
-const appUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/lgCIHcqWzTyEZ0MPUBMA/likes/';
-const endPoints = ['apps/','likes/','comments/'];
 
 const appId = 'YdXgSyeK6d2I6qrCBe1g/';
-
-const like = async() => {
-  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/likes/', {
-    method: 'POST',
-    body: JSON.stringify({
-      item_id: "1"
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  const response = await result.json();
-  return response;
-}
-
-const comments = async() => {
-  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/comments/', {
-    method:'POST',
-    body: JSON.stringify({
-      item_id: '1',
-      username: 'tomi',
-      comment: 'commentarioskldasd',
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  const response = await result.json();
-  return response;
-}
-
-const getComments = async() => {
-  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/comments?item_id=1', {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-    });
-    const response = result.json();
-    return response;
-}
-
-const getLikes = async() => {
-  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/likes/', {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  const response = await result.json();
-  return response;
-}
-
-//like()
-console.log(getLikes());
-console.log(getComments())
--- src/modules/dataTest.js
+++ src/modules/dataTest.js
@@ -5,13 +5,56 @@ const getData = async (url) => {
   return data;
 };
 
-const setLike = async (url,id) => {
-  const result = await fetch(url, {
+const setLike = async(itemId) => {
+  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/likes/', {
     method: 'POST',
-    body: JSON.stringify({item_id: id})
+    body: JSON.stringify({
+      item_id: itemId,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   });
   const response = await result.json();
   return response;
 }
 
-export { getData, setLike };
+const setComment = async(itemId, userName, userComment) => {
+  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/comments/', {
+    method:'POST',
+    body: JSON.stringify({
+      item_id: itemId,
+      username: `${userName}`,
+      comment: `${userComment}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const response = await result.json();
+  return response;
+}
+
+const getComments = async(itemId) => {
+  const result = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/comments?item_id=${itemId}`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    });
+    const response = result.json();
+    return response;
+}
+
+const getLikes = async() => {
+  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lgCIHcqWzTyEZ0MPUBMA/likes/', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const response = await result.json();
+  return response;
+}
+
+export { getData, setLike, getLikes, setComment, getComments };
