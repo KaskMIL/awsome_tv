062c8290d46fe710bbe1d5f71d15aeafc5e6db3b
worashf <uppert83@gmail.com>
2022-06-22 16:51:30 +0300

    change import to default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles/main.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import { retriveAllData } from './module/dom-utils';
+import retriveAllData from './module/dom-utils.js';
 
-window.onload= ()=>retriveAllData();
+window.onload = () => retriveAllData();
