2363bf2b31d8a2a627b5ec216ba5df17e8cc2be4
toMilanesi <tomi.milanesi@gmail.com>
2022-06-22 09:57:27 -0300

    Add data_interaction file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/main.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import { getData, setLike, getLikes } from './modules/dataTest.js';
+import { getData, setLike, getLikes } from './modules/data_interaction.js';
 
 const mainUrl = 'https://api.tvmaze.com/shows';
 
