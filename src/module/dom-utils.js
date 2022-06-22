ee872c6d03426bc1c579a0757494250d5cedc37c
worashf <uppert83@gmail.com>
2022-06-22 16:53:10 +0300

    add like structure

--- src/module/dom-utils.js
+++ src/module/dom-utils.js
@@ -1,35 +1,31 @@
+import { API_URL, tvWrapper } from './constant.js';
+import getData from './api-util.js';
 
- import { API_URL ,tvWrapper} from "./constant.js";
-import { getData } from "./api-util.js";
-
-const displayAllData =(data)=>{
-    //reset  loaded elements
-    //  tvWrapper.innerHTML=''
-    console.log(tvWrapper)
-    console.log(data[0].name)
-     const filtered = data.slice(0,6);
-  
-     filtered.forEach(element => {
-     
-         tvWrapper.innerHTML += `
+const displayAllData = (data) => {
+  data.forEach((element) => {
+    tvWrapper.innerHTML += `
          <div class="card" id=${element.id}> 
-        
+           
            <img src=${element.image.medium} alt ="best" class="image"/>
+           <div class="show-info">
             <p class="show-name"> ${element.name}</p>
+            <div class="like">
+            <i class="fa fa-heart heart" aria-hidden="true"></i>
+            <div class="likes-num">
+               <span> ${data.length}</span> likes
+            </div>
+            </div>
+           </div>
          </div>
-         `
-     });
-}
+         `;
+  });
+};
 
+const retriveAllData = () => new Promise((resolve) => {
+  getData(API_URL).then((res) => {
+    displayAllData(res);
+    resolve();
+  });
+});
 
-export const  retriveAllData =()=> new Promise((resolve)=>{
- getData(API_URL).then((res)=>{
-displayAllData(res)
-   resolve();
- })
-})
-
-
-
-
-
+export default retriveAllData;
