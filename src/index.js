d3822fcf2b93cdad245e1ee1bac146ef200e5a6d
toMilanesi <tomi.milanesi@gmail.com>
2022-06-22 16:11:47 -0300

    Add function that generates pop-up dinamicly by id

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './styles/main.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import { getData, setLike, getLikes, setComment, getComments, displayComments } from './modules/data_interaction.js';
+import { createPopUp } from './modules/popUp_element.js';
 
-const mainUrl = 'https://api.tvmaze.com/shows';
+const mainUrl = 'https://api.tvmaze.com/shows/1';
 
 const appId = 'YdXgSyeK6d2I6qrCBe1g/';
+
+const genrecont = document.getElementById('genreContainer');
+const sumContainer = document.querySelector('.summary');
+const main = document.querySelector('.main');
+
+main.appendChild(createPopUp(5))
+
+
