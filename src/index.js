0871fc2a7ab9293191b5c07aea2a76212ffe8f09
worashf <uppert83@gmail.com>
2022-06-23 22:51:47 +0300

    add  show count function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './styles/main.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import { retriveAllData } from './modules/dom-utils.js';
+import { retriveAllData, countShows } from './modules/dom-utils.js';
 import setPopup from './modules/popUp_element.js';
 
 const main = document.querySelector('.main');
 
-window.onload = () => retriveAllData();
+window.onload = () => retriveAllData().then(() => {
+  countShows();
+});
 setPopup(main);
